fc1556dcaa4d2e2e6fc27950afec5470e3c8bb9c
Daniel W <danielitit@gmail.com>
2019-04-10 12:25:40 +0200

    Add pgbouncer role

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,13 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_env_vars_added(host):
+    f = host.file('/etc/pgbouncer/pgbouncer.ini')
+
+    assert f.exists
+    assert f.contains("VARIABLE='variable value'")
